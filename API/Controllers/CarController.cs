93753cbc14975c20d1031d41923ab11b86edbae6
Cristiano Cunha <contato@cristianoprogramador.com>
2025-01-10 09:40:59 -0300

    Cria controller de cricação de carro

---
+++ API/Controllers/CarController.cs
@@ -0,0 +1,18 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CarController : Controller
+{
+    [HttpPost]
+    public IActionResult Create([FromBody] Car car)
+    {
+        if (ModelState.IsValid is false)
+            return BadRequest(ModelState);
+
+        return Created(string.Empty, car);
+    }
+}
--- API/Models/Car.cs
+++ API/Models/Car.cs
@@ -5,9 +5,10 @@ namespace API.Models;
 [Schema(nameof(Car))]
 public class Car
 {
-    [Required] public string Name { get; set; }
+    [Required] 
+    public string Name { get; set; }
 
-    [RegularExpression("[A-Z]{3}[0-9][0-9A-Z][0-9]{2}")]
+    [RegularExpression("[A-Z]{3}[0-9][0-9A-Z][0-9]{2}", ErrorMessage = "Placa no formato incorreto.")]
     public string Plate { get; set; }
 
     public Company Manufacturer { get; set; }
