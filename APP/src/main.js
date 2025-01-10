7513427581f5cf9ae8b4e43cd402c8b7640de898
Cristiano Cunha <contato@cristianoprogramador.com>
2025-01-10 11:09:42 -0300

    Remove App

--- APP/src/main.js
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import { JsonForms } from '@jsonforms/vue'
-
-const app = createApp(App)
-app.component('json-forms', JsonForms)
-app.mount('#app')
