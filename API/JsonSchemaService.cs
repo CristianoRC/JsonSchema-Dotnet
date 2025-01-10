678070193c22358ea6033da1c8186a47fdaf4299
Cristiano Cunha <contato@cristianoprogramador.com>
2025-01-10 09:56:47 -0300

    Adiciona validação de duplicados ao iniciar a API

--- API/JsonSchemaService.cs
+++ API/JsonSchemaService.cs
@@ -18,4 +18,25 @@ public static class JsonSchemaService
         var schema = JsonSchema.FromType(schemaType);
         return schema.ToJson();
     }
+
+    public static void ValidateDuplicatedSchemaNames()
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+        var typesWithSchemas = assembly.GetTypes()
+            .SelectMany(t => t.GetCustomAttributes<SchemaAttribute>()
+            .Select(a => new { Type = t, SchemaName = a.Name }))
+            .GroupBy(x => x.SchemaName, StringComparer.OrdinalIgnoreCase);
+
+        var duplicates = typesWithSchemas
+            .Where(g => g.Count() > 1)
+            .ToList();
+
+        if (duplicates.Count == 0)
+            return;
+        
+        var duplicateMessages = duplicates.Select(g =>
+            $"Schema '{g.Key}' está sendo usado por múltiplos tipos: {string.Join(", ", g.Select(x => x.Type.Name))}");
+        var exceptionMessage = $"Foram encontrados schemas com nomes duplicados:\n{string.Join("\n", duplicateMessages)}";
+        throw new DuplicateSchemaException(exceptionMessage);
+    }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -4,18 +4,17 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        //Adicionado para validar que não temos schma names duplicados 
+        JsonSchemaService.ValidateDuplicatedSchemaNames();
+        
         var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
+        
         builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.UseSwagger();
