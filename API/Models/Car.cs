97c5da7e35c44572fa54f1f167c63f6d6cf7c91a
Cristiano Cunha <contato@cristianoprogramador.com>
2025-01-10 09:34:07 -0300

    Adiciona schema no car

--- API/Models/Car.cs
+++ API/Models/Car.cs
@@ -2,10 +2,10 @@
 
 namespace API.Models;
 
+[Schema(nameof(Car))]
 public class Car
 {
-    [Required]
-    public string Name { get; set; }
+    [Required] public string Name { get; set; }
 
     [RegularExpression("[A-Z]{3}[0-9][0-9A-Z][0-9]{2}")]
     public string Plate { get; set; }
