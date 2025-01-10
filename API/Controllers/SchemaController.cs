f142e8c1b85a0ac8443c150aacd1bce94813bab3
Cristiano Cunha <contato@cristianoprogramador.com>
2025-01-10 09:03:38 -0300

    Remove controler de exemplo

---
+++ API/Controllers/SchemaController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SchemaController : Controller
+{
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
